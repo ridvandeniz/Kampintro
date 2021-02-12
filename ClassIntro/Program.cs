dd5356e0d9837b44e24a86cf7ce05cf7d87eb949
RIDVAN <ridvanramazandeniz@gmail.com>
2021-02-12 23:04:00 +0300

    Proje dosyası ekle.

---
+++ ClassIntro/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace ClassIntro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            kurs kurs1 = new kurs();
+            kurs1.Egitmen = "Rıdvan";
+            kurs1.KursAdi = "C#";
+            kurs1.izlenmeOranı = 80 ;
+
+            kurs kurs2 = new kurs();
+            kurs2.Egitmen = "Ahmet";
+            kurs2.KursAdi = "Java";
+            kurs2.izlenmeOranı = 58 ;
+
+            kurs kurs3 = new kurs();
+            kurs3.Egitmen = "Mehmet";
+            kurs3.KursAdi = "C++";
+            kurs3.izlenmeOranı = 65 ;
+
+            Console.WriteLine( " % " + kurs1.izlenmeOranı );
+        }
+    }
+
+    class kurs
+    {
+        public string KursAdi { get; set; }
+        public string Egitmen { get; set; }
+        public int izlenmeOranı { get; set; }
+    }
+}
---
+++ Matematik/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Matematik
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Dortislem dortislem = new Dortislem();
+            
+            dortislem.topla(5, 3);
+        }
+    }
+}
